56af1edd4aed5665b0ae22cc0497fc32589d6b61
debamitra <debamitra.mukherjee@gmail.com>
2020-08-10 05:15:43 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ceaa0cb5554e351937c5.js"
+    "url": "webpack-runtime-f371955a2e957ea13fcb.js"
   },
   {
     "url": "framework-b5b229d58c3c710d5429.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8302329334da0af1181424ffeb57fec0"
+    "revision": "6168c3ba65d83631c6b8cc258a005c1b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "00aeec02317fbc69fcce1c0ee74fc9f4"
+    "revision": "3b4335b9fb7e4cf1e9caa4c353bc8374"
   },
   {
     "url": "polyfill-d10938940af3acfd070f.js"
